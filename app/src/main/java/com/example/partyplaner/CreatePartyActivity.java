7f2f34ba8dab31aa70bc2d2a8cba49149a5d01b4
Žan Plahuta <79518305+zanplahuta@users.noreply.github.com>
2022-03-10 16:15:03 +0100

    Izbira in prikaz slike

--- app/src/main/java/com/example/partyplaner/CreatePartyActivity.java
+++ app/src/main/java/com/example/partyplaner/CreatePartyActivity.java
@@ -1,40 +1,82 @@
 package com.example.partyplaner;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 
 public class CreatePartyActivity extends AppCompatActivity {
     private static final String TAG = CreatePartyActivity.class.getSimpleName();
 
-    public static final String FORM_MODE_ID = "FORM_MODE_ID";
-    public static final int FORM_MODE_INSERT = 0;
-
-    public static final int ACTIVITY_ID = 111;
-    public static final String DATA_ACTION = "DATA_ACTION";
+    private static final int IMAGE_SELECT_CODE = 900;
 
     protected Parties parties;
     SharedPreferences sharedPreferences;
 
     Button buttonDiscard, buttonCreate, buttonChooseFile;
-    EditText etTitle, etDescription;
-    DatePicker etDate;
+    EditText etTitle, etDescription, etDate;
+    TextView textViewURI;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_party);
+
+        imageView = findViewById(R.id.imageView);
+        buttonDiscard = findViewById(R.id.buttonDiscard);
+        buttonCreate = findViewById(R.id.buttonCreate);
+        buttonChooseFile = findViewById(R.id.buttonChooseFile);
+        etTitle = findViewById(R.id.etTitle);
+        etDescription = findViewById(R.id.etDescription);
+        etDate = findViewById(R.id.etDate);
+        textViewURI = findViewById(R.id.textViewURI);
+
+        buttonChooseFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, IMAGE_SELECT_CODE);
+            }
+        });
     }
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_SELECT_CODE && resultCode == RESULT_OK){
+            if (data != null){
+                Uri uri = data.getData();
+                imageView.setImageURI(uri);
+                textViewURI.setText((String)imageView.getTag());
+            }
+        }
     }
+
 }
